4fb76a913b50c09952e2172139846265cc97ce84
dennism0027 <33007602+dennism0027@users.noreply.github.com>
2017-11-28 19:44:20 -0600

    Add files via upload

---
+++ BDA+final+project+data+normilzation.py
@@ -0,0 +1,47 @@
+
+# coding: utf-8
+
+# In[1]:
+
+
+import numpy as np
+import pandas as pd
+
+
+# In[2]:
+
+
+def loadDataset(filename):
+    data = pd.read_csv(filename, header=None)
+    return data
+
+
+# In[3]:
+
+
+def normilizeDataset(data):
+    norm = np.sqrt(np.square(data.iloc[:,1:28]).sum(axis=1))
+    newdata = data
+    for row in range(0, data.shape[0]):
+        for column in data.columns[1:]:
+            newdata.iloc[row,column] = data.iloc[row,column] / norm[row]
+    return newdata
+
+
+# In[4]:
+
+
+def main():
+    file = 'HIGGS.csv'
+    
+    
+    data = loadDataset(file)
+    newdata = normilizeDataset(data)
+    newdata.to_csv('newdata.csv', encoding='utf-8', index=False)
+
+
+# In[ ]:
+
+
+main()
+
